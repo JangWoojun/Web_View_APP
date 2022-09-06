9a84485dce9ba89446b6d4892dae00027dd33eb3
JangWoojun <woojun0107@naver.com>
2022-09-06 21:42:01 +0900

    2022/09/06 WebView 적용, 콘텐츠 추가 , 디자인 수정

--- app/src/main/java/com/example/mango_contents/MainActivity.kt
+++ app/src/main/java/com/example/mango_contents/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.mango_contents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -44,22 +46,117 @@ class MainActivity : AppCompatActivity() {
         )
         items.add(
             ContentModel(
-                "https://www.mangoplate.com/restaurants/RLwmEULwZPHo",
-                "https://mp-seoul-image-production-s3.mangoplate.com/288424_1656598616585466.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
-                "금원함흥냉면"
+                "https://www.mangoplate.com/restaurants/VdkLLgPWWaCe",
+                "https://www.mangoplate.com/restaurants/VdkLLgPWWaCe",
+                "필구커피"
             )
         )
         items.add(
             ContentModel(
-                "https://www.mangoplate.com/restaurants/r0oHu19ooXxW",
-                "https://mp-seoul-image-production-s3.mangoplate.com/430689/439642_1660275196148_3936?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
-                "YM에스프레소룸"
+                "https://www.mangoplate.com/restaurants/OSyIG-aiK2u_",
+                "https://mp-seoul-image-production-s3.mangoplate.com/617296_1594644170549462.jpg?fit=around|512:512&crop=512:512;*,*&output-format=jpg&output-quality=80",
+                "오레노라멘"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/Y8rBeDQx300W",
+                "https://www.mangoplate.com/restaurants/Y8rBeDQx300W",
+                "햇살머무는식탁"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/OybhNJprdbjV",
+                "https://www.mangoplate.com/restaurants/OybhNJprdbjV",
+                "히카리우동"
+            )
+        )
+        items.add(
+            ContentModel(
+                "mangoplate.com/restaurants/IH9yXPcXK9",
+                "https://mp-seoul-image-production-s3.mangoplate.com/1704015_1647292401380684.jpg?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80",
+                "삼오순대국"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/2BvZsuhVXBiA",
+                "https://mp-seoul-image-production-s3.mangoplate.com/1757039_1629718218737542.jpg?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80",
+                "포옹싸 "
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/jfx6vNvQPwf-",
+                "https://mp-seoul-image-production-s3.mangoplate.com/1047892_1629374260979864.jpg?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80",
+                "동"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/ZU5yw9Q_SAu1",
+                "https://mp-seoul-image-production-s3.mangoplate.com/1609150_1581753452281729.jpg?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80",
+                "온도차"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/_ObtBp2gTr",
+                "https://mp-seoul-image-production-s3.mangoplate.com/197741/45556_1626173533189_16397?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80",
+                "성너머집"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/bZiVaUmeNZ8t",
+                "https://mp-seoul-image-production-s3.mangoplate.com/1095722_1633774976507833.jpg?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80",
+                "코스믹버거랩"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/i2Wsu9MZl9",
+                "https://mp-seoul-image-production-s3.mangoplate.com/1515134_1619767984538700.jpg?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80",
+                "원조두꺼비불오징어 "
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/hRxG1GICPwm0",
+                "https://mp-seoul-image-production-s3.mangoplate.com/1841276_1649517303492758.jpg?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80",
+                "플롭"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/nsC2gslUeJ",
+                "https://mp-seoul-image-production-s3.mangoplate.com/91729/439642_1597292077911_8009?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80",
+                "목노집"
+            )
+        )
+        items.add(
+            ContentModel(
+                "https://www.mangoplate.com/restaurants/ycHaFj1ZlG",
+                "https://mp-seoul-image-production-s3.mangoplate.com/350696/1523303_1630114603062_39848?fit=around|738:738&crop=738:738;*,*&output-format=jpg&output-quality=80",
+                "벙구갈비"
             )
         )
 
         val recyclerview = findViewById<RecyclerView>(R.id.rv)
         val rvAdapter = RVAdapter(baseContext, items)
         recyclerview.adapter = rvAdapter
+
+        rvAdapter.itemClick = object: RVAdapter.ItemClick {
+            override fun onClick(view: View, position: Int) {
+
+                val intent = Intent(baseContext, ViewActivity::class.java)
+                intent.putExtra("url",items[position].url)
+                startActivity(intent)
+
+            }
+
+        }
 
         recyclerview.layoutManager = GridLayoutManager(this,2)
     }
