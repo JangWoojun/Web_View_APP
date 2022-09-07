56c03998d3e94a092d0430ed87b471b49aec086b
JangWoojun <woojun0107@naver.com>
2022-09-08 04:13:39 +0900

    2022/09/08 login -> join으로 이름변경, 회원가입 구현 완료

--- app/src/main/java/com/example/mango_contents/SplashActivity.kt
+++ app/src/main/java/com/example/mango_contents/SplashActivity.kt
@@ -4,16 +4,35 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        Handler().postDelayed({
-            startActivity(Intent(this,MainActivity::class.java))
-            finish()
-        }, 2000)
+        auth = Firebase.auth
+
+        if (auth.currentUser?.uid == null){
+            //회원가입 안됐으니 join으로
+            Handler().postDelayed({
+                startActivity(Intent(this,JoinActivity::class.java))
+                finish()
+            }, 2000)
+        }
+        else {
+            //회원 가입 됨
+            Handler().postDelayed({
+                startActivity(Intent(this,MainActivity::class.java))
+                finish()
+            }, 2000)
+        }
 
     }
 }
